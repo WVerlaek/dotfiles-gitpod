4c12bd72ac88e22a4da562b474d71420c311f456
Wouter Verlaek <wouter@gitpod.io>
2023-01-04 10:13:16 +0000

    Zsh

--- .bash_aliases
+++ .bash_aliases
@@ -1,8 +1,8 @@
 source ~/gcloud/.alias
-source ~/git/.alias
+# source ~/git/.alias
 source ~/gitpod/.alias
 source ~/kubernetes/.alias
-source ~/system/.alias 
+source ~/system/.alias
 
 source ~/gitpod/.functions
 source ~/kubernetes/.functions
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## Credits: https://github.com/easyCZ/dotfiles/blob/master/setup.sh
+set -ex
+
+echo "Setting up dotfiles"
+
+if [[ -z "$ZSH_VERSION" ]]; then
+    echo "Installing zsh"
+    sudo apt install zsh
+fi
+
+if [[ ! -d "~/.oh-my-zsh" ]]; then
+    echo "Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Install spaceship
+# https://github.com/spaceship-prompt/spaceship-prompt
+ZSH_CUSTOM="/home/gitpod/.oh-my-zsh/custom"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+if [[ -d "/home/gitpod/.dotfiles" ]]; then
+    # In gitpod, dotfiles are stored in this directory
+    pushd "/home/gitpod/.dotfiles"
+        mv .zshrc ~/.zshrc
+    popd
+fi
+
+export SHELL=zsh
